4d03b8e04a9cd1ad87f9f4ea9334198f42cd4e79
Jennifer Tang jenntangy@gmail.com <jenntangy@gmail.com>
2017-06-06 13:40:48 +0200

    The delete and show work, still working on the edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
+  get 'tasks', to: 'tasks#index'
 
-  get 'tasks/show'
+  post 'tasks', to: 'tasks#create'
 
-  get 'tasks/new'
+  get 'tasks/new', to: 'tasks#new'
 
-  get 'tasks/create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
 
-  get 'tasks/edit'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 
-  get 'tasks/update'
+  patch 'tasks/:id', to: 'tasks#update'
 
-  get 'tasks/destroy'
+  delete 'tasks/:id', to: 'tasks#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+
+
+
